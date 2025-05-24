693221aa5fc7c54dd73d462688f34ccfa9e4674d
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2025-05-24 18:46:21 +0200

    Mealy

--- WaterPump/waterpump.vhd
+++ WaterPump/waterpump.vhd
@@ -13,31 +13,28 @@ end waterpump;
 architecture Behavior of waterpump is
     constant MAX_LVL : integer := 50;
     constant MIN_LVL : integer := 40;
-    signal low_counter  : integer range 0 to 3 := 0;
-    signal high_counter : integer range 0 to 3 := 0;
+    signal low_counter  : integer := 0;
+    signal high_counter : integer := 0;
 begin
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if water_level < MIN_LVL then
-                if low_counter < 3 then
-                    low_counter <= low_counter + 1;
-                end if;
-            else
-                low_counter <= 0;
-            end if;
-            if water_level > MAX_LVL then
-                if high_counter < 3 then
-                    high_counter <= high_counter + 1;
-                end if;
-            else
-                high_counter <= 0;
-            end if;
-            if low_counter = 3 then
-                pump_state <= '1';
-            elsif high_counter = 3 then
-                pump_state <= '0';
-            end if;
+process(clk)
+begin
+    if rising_edge(clk) then
+        if water_level < MIN_LVL then
+            low_counter <= low_counter + 1;
+        else
+            low_counter <= 0;
         end if;
-    end process;
+		if water_level > MAX_LVL then
+			high_counter <= high_counter + 1;
+        else   
+             high_counter <= 0;
+        end if;
+    end if;
+
+    if water_level < MIN_LVL and low_counter > 3 then
+        pump_state <= '1';
+    elsif water_level > MAX_LVL and high_counter > 3 then
+        pump_state <= '0';
+    end if;
+end process;
 end Behavior;
